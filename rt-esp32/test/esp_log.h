753bfcd7eb38f272c323869bbf73671e30fa3948
shakilapr <shakilabeta@gmail.com>
2026-06-16 19:45:08 +0530

    fix: clean up MtrMotorFbk struct placement (linter will resolve)

---
+++ rt-esp32/test/esp_log.h
@@ -0,0 +1,5 @@
+#pragma once
+#define ESP_LOGD(tag, fmt, ...) ((void)0)
+#define ESP_LOGI(tag, fmt, ...) ((void)0)
+#define ESP_LOGW(tag, fmt, ...) ((void)0)
+#define ESP_LOGE(tag, fmt, ...) ((void)0)
