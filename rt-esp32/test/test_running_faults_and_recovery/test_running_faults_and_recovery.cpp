636adcc5abdb485ea62dc4d48c2c8d3310bb1736
shakilapr <shakilabeta@gmail.com>
2026-09-10 11:42:55 +0530

    test(rt): restore all native firmware suites

--- rt-esp32/test/test_running_faults_and_recovery/test_running_faults_and_recovery.cpp
+++ rt-esp32/test/test_running_faults_and_recovery/test_running_faults_and_recovery.cpp
@@ -4,13 +4,11 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
-
-#include "stub/stm32g4xx_hal.h"
+#include "stm32g4xx_hal.h"
 #include "protocol/compat/can.hpp"
 #include "protocol/generated/cpp/etrike_protocol.hpp"
 #include "protocol/codecs/ses.hpp"
 #include "protocol/codecs/seb.hpp"
-
 #include "physics_model.h"
 #include "brake_arbitration.h"
 #include "seb_request.h"
